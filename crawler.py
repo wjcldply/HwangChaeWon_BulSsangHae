f112b8cb4deb62b3350f6557cbe576c721737fb7
Wonjin Kim <wjkim9653@gmail.com>
2022-09-09 01:29:30 +0900

    1. deleted main.py 2. added codes to sort the data and save as csv

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
+import csv
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -12,7 +14,13 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
+items = []  # save the item infos
+last_key = 0  # save the identification number of the last item saved
+
+
 def table_to_lst(out_lst, in_elements):
+    global last_key
+    # print(last_key)
     for element in in_elements:
         infos = element.find_elements(By.CLASS_NAME, 'table_txt')
         temp = []  # temporarily save the item infos without sorting
@@ -30,10 +38,27 @@ def table_to_lst(out_lst, in_elements):
                 new_item.append(temp[i])
             else:
                 new_item.append(temp[i])
-                out_lst.append(new_item)
+                if last_key == 0:
+                    out_lst.append(new_item)
+                    last_key = int(new_item[0])
+                elif int(new_item[0]) < last_key:
+                    out_lst.append(new_item)
+                    last_key = int(new_item[0])
+                else:
+                    pass
                 new_item = []
-    # pprint(items)
+    pprint(items)
     return out_lst
+
+
+f = open('식품.csv', 'w', newline='', encoding='utf-8-sig')
+writer = csv.writer(f)
+
+
+def items_to_csv():
+    global items
+    writer.writerows(items)
+
 
 # Setup options
 option = Options()
@@ -64,36 +89,21 @@ search_box.send_keys(search_keyword)
 # Choose to show 50 items per load
 browser.find_element(By.ID, 'a_list_cnt').click()
 browser.find_element(By.XPATH, '//*[@id="contents"]/main/section/div[2]/div[2]/div[2]/div[5]/ul/li[5]/a').click()
-'''
-bsObj = BeautifulSoup(browser, "html.parser")
-print(bsObj)
-'''
+
 # wait for the search result
 time.sleep(20)
 
 # save the whole table into <elements>
 elements = browser.find_elements(By.ID, 'tbody')
 
-# save the item infos
-items = []
-
+# save the data from table into a list
 table_to_lst(items, elements)
-'''
-for element in elements:
-    infos = element.find_elements(By.CLASS_NAME, 'table_txt')
-    temp = []  # temporarily save the item infos without sorting
-    for info in infos:
-        temp.append(info.text)
-    new_item = []  # sort the infos from table
-    for i in range(len(temp)):
-        if len(new_item) != 5:
-            new_item.append(temp[i])
-        else:
-            items.append(new_item)
-            new_item = []
-pprint(items)
-'''
-end_page_num = 5
+
+items_to_csv()  # 이중 리스트 items에 담긴 정보들을 csv에 저장하는 함수 작성하기
+items = []  # items 초기화해 리스트 오버플로우 방지하기
+
+end_page_num = 2221
+# end_page_num = 3
 for i in range(1, end_page_num):
     # load the next page
     browser.find_element(By.XPATH, '//*[@id="contents"]/main/section/div[2]/div[3]/div/ul/li[7]').click()
@@ -102,17 +112,14 @@ for i in range(1, end_page_num):
 
     # save the whole table into <elements>
     elements = browser.find_elements(By.ID, 'tbody')
-
+    # save the data from table into a list
     table_to_lst(items, elements)
+
+    items_to_csv()  # 이중 리스트 items에 담긴 정보들을 csv에 저장하는 함수 작성하기
+    items = []  # items 초기화해 리스트 오버플로우 방지하기 
+
+
 
 pprint(items)
 print('total number of items searched is :' + str(len(items)))
-'''
-for i in range(1, end_page_num):
-    browser.find_element(By.XPATH, '//*[@id="contents"]/main/section/div[2]/div[3]/div/ul/li[7]').click()
-    time.sleep(20)
-    elements = browser.find_elements(By.ID, 'tbody')
-    for element in elements:
-        print(type(element))
-        pprint(element.text)
-'''
+f.close()
