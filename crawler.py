44091074611b9ddfcb2d3542abf5792f193c6884
Wonjin Kim <58078984+wjcldply@users.noreply.github.com>
2022-09-09 22:53:36 +0900

    Update crawler.py
    added and tested a new function -> check_expiration(list)
      - checks the expiration data with keyword '개월' and '년' > create a new .csv file that only saves the filtered data.

--- crawler.py
+++ crawler.py
@@ -17,6 +17,7 @@ import time
 items = []  # save the item infos
 last_key = 0  # save the identification number of the last item saved
 item_count = 0
+filtered_item_count = 0
 
 
 def table_to_lst(out_lst, in_elements):
@@ -52,8 +53,10 @@ def table_to_lst(out_lst, in_elements):
     return out_lst
 
 
-f = open('식품.csv', 'w', newline='', encoding='utf-8-sig')
-writer = csv.writer(f)
+f_raw = open('식품_raw.csv', 'w', newline='', encoding='utf-8-sig')
+f_filtered = open('식품_filtered.csv', 'w', newline='', encoding='utf-8-sig')
+writer = csv.writer(f_raw)
+writer2 = csv.writer(f_filtered)
 
 
 def items_to_csv():
@@ -61,6 +64,42 @@ def items_to_csv():
     global item_count
     writer.writerows(items)
     item_count += len(items)
+
+
+def check_expiration(lst):  # 유통기한 필터링 
+    global filtered_item_count
+    new_line = lst
+    expiration_date_unformatted = lst[4]
+    # '개월' 키워드로 찾기
+    idx = expiration_date_unformatted.find('개월')
+    if idx == -1:  # '개월' 키워드 존재하지 않을 경우
+        # '년' 키워드로 찾기
+        idx = expiration_date_unformatted.find('년')
+        if idx == -1:  # '년' 키워드 존재하지 않을 경우
+            return
+        else:
+            try:
+                digit = int(expiration_date_unformatted[idx-2:idx])
+            except:
+                digit = int(expiration_date_unformatted[idx-1])
+            if digit >= 2:
+                new_line[4] = digit
+                writer2.writerow(new_line)
+                filtered_item_count += 1
+                return
+    else:
+        try:
+            digit = int(expiration_date_unformatted[idx-2:idx])
+        except:
+            digit = int(expiration_date_unformatted[idx-1])
+        if digit >= 24:
+            new_line[4] = digit
+            writer2.writerow(new_line)
+            filtered_item_count += 1
+            return
+        else:
+            return
+        
 
 
 # Setup options
@@ -102,11 +141,13 @@ elements = browser.find_elements(By.ID, 'tbody')
 # save the data from table into a list
 table_to_lst(items, elements)
 
-items_to_csv()  # 이중 리스트 items에 담긴 정보들을 csv에 저장하는 함수 작성하기
+items_to_csv()  # 이중 리스트 items에 담긴 정보들을 csv에 저장하는 함수
+for item in items:
+    check_expiration(item)  # 크롤링한 테이블의 식품 유통기한 필터링하는 함수
 items = []  # items 초기화해 리스트 오버플로우 방지하기
 
-end_page_num = 2221
-# end_page_num = 3
+# end_page_num = 2221
+end_page_num = 3
 for i in range(1, end_page_num):
     # load the next page
     browser.find_element(By.XPATH, '//*[@id="contents"]/main/section/div[2]/div[3]/div/ul/li[7]').click()
@@ -119,10 +160,15 @@ for i in range(1, end_page_num):
     table_to_lst(items, elements)
 
     items_to_csv()  # 이중 리스트 items에 담긴 정보들을 csv에 저장하는 함수 작성하기
+    for item in items:
+        check_expiration(item)  # 크롤링한 테이블의 식품 유통기한 필터링하는 함수
     items = []  # items 초기화해 리스트 오버플로우 방지하기 
+
 
 
 
 # pprint(items)
 print('total number of items searched is :' + str(item_count))
-f.close()
+print('total number of filtered items is :' + str(filtered_item_count))
+f_raw.close()
+f_filtered.close()
